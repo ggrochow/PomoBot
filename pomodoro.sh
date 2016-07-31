14d070d81ba412e9148ebda725d87fe2d0519a2c
Graham Grochowski <ggrochow@gmail.com>
2016-07-30 22:51:24 -0700

    futher config

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+npm install #--no-optional
+#npm run -s migrate &&
 npm start
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 // Run dotenv so we can use .env file, do this before anything
 require('dotenv').config();
 require('./app.js');
-// require('./models/pomodoro.js');
-// require('./postgres.js');
